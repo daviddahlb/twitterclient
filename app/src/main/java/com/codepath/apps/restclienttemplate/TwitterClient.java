9d4fe8e7277a1089b5d3d89455d16c458c77eb54
daviddahlb <davidsdahlberg@gmail.com>
2019-02-24 12:36:27 -0800

    Compose tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TwitterClient.java
@@ -51,12 +51,21 @@ public class TwitterClient extends OAuthBaseClient {
 		params.put("since_id", 1);
 		client.get(apiUrl, params, handler);
 	}
+
 	public void getNextTimeline(AsyncHttpResponseHandler handler, long oldestTweetId){
 		String apiUrl = getApiUrl("statuses/home_timeline.json");
 		RequestParams params = new RequestParams();
 		params.put("count", 25);
 		params.put("max_id", oldestTweetId);
 		client.get(apiUrl, params, handler);
+	}
+
+	public void composeTweet(String tweetContent, AsyncHttpResponseHandler handler) {
+		String apiUrl = getApiUrl("statuses/update.json");
+		// Can specify query string params directly or through RequestParams.
+		RequestParams params = new RequestParams();
+		params.put("status", tweetContent);
+		client.post(apiUrl, params, handler);
 	}
 
 	/* 1. Define the endpoint URL with getApiUrl and pass a relative path to the endpoint
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -1,14 +1,30 @@
 package com.codepath.apps.restclienttemplate.models;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
+@Entity
 public class User {
 
+    @ColumnInfo
     public String name;
+    @ColumnInfo
+    @PrimaryKey(autoGenerate = true)
     public long uid;
+    @ColumnInfo
     public String screenName;
+    @ColumnInfo
     public String profileImageUrl;
+
+    // Empty constructor for Parceler Library
+    public User() {
+    }
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
